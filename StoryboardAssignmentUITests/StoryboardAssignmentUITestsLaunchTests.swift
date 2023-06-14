ec675742dcb2d4480cdaf90b05ce80f15908bf87
bmalkhare-mdsol <bmalkhare@mdsol.com>
2023-06-14 12:44:51 +0530

    Initial Commit

---
+++ StoryboardAssignmentUITests/StoryboardAssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StoryboardAssignmentUITestsLaunchTests.swift
+//  StoryboardAssignmentUITests
+//
+//  Created by Bhakti MALKHARE on 14/06/23.
+//
+
+import XCTest
+
+final class StoryboardAssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
